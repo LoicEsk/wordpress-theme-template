9837e986863d877539857cd10ef57f2a7f8363fe
Loïc Laurent <llaurent@selexium.com>
2025-12-08 13:37:53 +0100

    Theme enfant de Twenty Twenty-Five

---
+++ patterns/post-meta.php
@@ -0,0 +1,11 @@
+<?php
+
+?>
+<!-- wp:post-meta {"style":{"spacing":{"blockGap":"5px"}},"layout":{"type":"flex","orientation":"horizontal","justifyContent":"left","flexWrap":"nowrap"}} -->
+<div class="wp-block-post-meta" style="gap:5px">
+    <!-- wp:post-date {"format":"F j, Y","style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"textColor":"contrast-3","fontSize":"small"} /-->
+    <!-- wp:post-terms {"term":"category","style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"textColor":"contrast-3","fontSize":"small"} /-->
+</div>
+<!-- /wp:post-meta -->
+
+
